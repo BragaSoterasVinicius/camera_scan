6804c5aede3f1baa5c117a0f863f1b330829b0dd
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-05-20 20:45:12 -0300

    comportamento para contagem de detecçâo adicionado

--- camera_scan.bat
+++ camera_scan.bat
@@ -3,11 +3,15 @@ set numeroDeCameras = 0
 :proximaPorta
 	::chamada ffmpeg com a posicaoIp concatenada na ponta
 	if !errorlevel! NEQ 0(
-		set /a numeroDeCameras += 1
 		set /a posicaoIp +=1
 		goto proximaPorta
 	)|| if proximaPorta == 15(
 		echo 'nenhuma câmera está conectada ao wifi correto'
+	)
+	else if !errorlevel! == 0(
+		set /a numeroDeCameras += 1		
+		::lógica para abrir o programa de exibição das câmeras
+		goto proximaPorta	
 	)
 echo "Wifi escaneado, %numeroDeCameras% câmeras conectadas."
 pause
