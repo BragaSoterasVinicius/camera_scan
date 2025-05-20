9d68f000dbfc66f00a138908906ca7c6e0b25d04
Soteras <viniciussoteras@gmail.com>
2025-05-20 15:49:47 -0300

    Add files via upload

---
+++ camera_scan.bat
@@ -0,0 +1,14 @@
+set posicaoIp = 0
+set numeroDeCameras = 0
+:proximaPorta
+	::chamada ffmpeg com a posicaoIp concatenada na ponta
+	if !errorlevel! NEQ 0(
+		set /a numeroDeCameras += 1
+		set /a posicaoIp +=1
+		goto proximaPorta
+	)|| if proximaPorta == 15(
+		echo 'nenhuma câmera está conectada ao wifi correto'
+	)
+echo "Wifi escaneado, %numeroDeCameras% câmeras conectadas."
+pause
+
